eea1f69f2ecb34d5384c3c52a890b8dc3c32d773
Raimondas <raima220@gmail.com>
2022-01-26 16:52:02 +0000

    first commit

---
+++ src/utils/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbFn1xVs1JoHMjQMKegn8Jtab-Q5C3oX8",
+  authDomain: "piksel-512fa.firebaseapp.com",
+  projectId: "piksel-512fa",
+  storageBucket: "piksel-512fa.appspot.com",
+  messagingSenderId: "359961571838",
+  appId: "1:359961571838:web:639ba0a7d2adf3f155d0af"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
